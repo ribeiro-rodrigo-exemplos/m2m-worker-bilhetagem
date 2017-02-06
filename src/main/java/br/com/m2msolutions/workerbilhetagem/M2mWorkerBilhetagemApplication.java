e7fff91e714d9e0ec4fab89b91a137b7cbc0b2c5
Rodrigo Ribeiro <rodrigo.rs1989@gmail.com>
2017-02-06 17:20:26 -0200

    enviando estrutura inicial do projeto

---
+++ src/main/java/br/com/m2msolutions/workerbilhetagem/M2mWorkerBilhetagemApplication.java
@@ -0,0 +1,12 @@
+package br.com.m2msolutions.workerbilhetagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2mWorkerBilhetagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2mWorkerBilhetagemApplication.class, args);
+	}
+}
